e4461ed0031720098bfb7edd7638f14fd8d0d6fc
danielkrex <danielkrex@gmail.com>
2025-01-30 11:51:49 +0100

    config

--- vite.config.ts
+++ vite.config.ts
@@ -7,18 +7,27 @@ import tailwind from 'tailwindcss'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwind(), autoprefixer()],
+export default defineConfig(({ command, mode }) => {
+  const config = {
+    css: {
+      postcss: {
+        plugins: [tailwind(), autoprefixer()],
+      },
     },
-  },
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    base: '/' // default base for regular build
   }
+
+  if (mode === 'deploy') {
+    config.base = '/files/dpleadsheets/app/'
+  }
+
+  return config
 })
